8bdd277edbfa389bb21f905e4cbf1b19d38b5231
Paweł Wachol <wachol1991@o2.pl>
2024-02-06 16:19:57 +0100

    06.02.2024

---
+++ FR_15_01 - Nowe buty do biegania/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace FR_15_01___Nowe_buty_do_biegania
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double k, m = 1.609344;
+            k = double.Parse(Console.ReadLine());
+            Console.WriteLine(k / m < 300 ? "NIE" : ((k / m > 300) && (k / m < 500)) ? "SPRAWDZIMY TWOJE OBECNE BUTY" : "TAK");
+        }
+    }
+
+}
