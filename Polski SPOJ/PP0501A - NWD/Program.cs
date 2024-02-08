3d968932919e9c48e012345205e6720bb8aa0d4e
Paweł Wachol <wachol1991@o2.pl>
2024-02-08 08:55:47 +0100

    08.02.2024

---
+++ Polski SPOJ/PP0501A - NWD/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace PP0501A___NWD
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ile_t = int.Parse(Console.ReadLine());
+            for (int i = 0; i < ile_t; i++)
+            {
+                string input = Console.ReadLine();
+                string[] parts = input.Split(' ');
+                int a = int.Parse(parts[0]);
+                int b = int.Parse(parts[1]);
+
+                int nwd = NWD(a, b);
+                Console.WriteLine(nwd);
+            }
+        }
+        static int NWD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int x = b;
+                b = a % b;
+                a = x;
+            }
+            return a;
+        }
+    }
+
+}
