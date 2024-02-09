332277458408bc67159029b7505a8b6a85c9e3c3
Paweł Wachol <123093109+PawelW91@users.noreply.github.com>
2024-02-09 15:17:47 +0100

    Update Program.cs

--- Polski SPOJ/AL_06_01 - Reszta z dzielenia/Program.cs
+++ Polski SPOJ/AL_06_01 - Reszta z dzielenia/Program.cs
@@ -1,1 +1,21 @@
-﻿
+namespace AL_06_01___Reszta_z_dzielenia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ile_t = int.Parse(Console.ReadLine());
+            while (ile_t-->0)
+            {
+                string input = Console.ReadLine();
+                string[] parts = input.Split(' ');
+                int a = int.Parse(parts[0]);
+                int b = int.Parse(parts[1]);
+                int r;
+                r=a%b;
+                if (r < 0) { r += Math.Abs(b); Console.WriteLine(r); }
+                else { Console.WriteLine(r); }
+            }
+        }
+    }
+}﻿
