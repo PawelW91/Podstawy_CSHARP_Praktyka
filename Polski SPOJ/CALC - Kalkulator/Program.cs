c307af2d977733cb58780c9362da25500dbac609
Paweł Wachol <123093109+PawelW91@users.noreply.github.com>
2024-02-10 12:12:09 +0100

    10.02.2024

--- Polski SPOJ/CALC - Kalkulator/Program.cs
+++ Polski SPOJ/CALC - Kalkulator/Program.cs
@@ -1,1 +1,31 @@
-﻿
+namespace CALC___Kalkulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input="";
+            bool koniec = false;
+            while (!koniec)
+            {
+                input = Console.ReadLine();
+                if (input !=null && input!="")
+                {
+                    string[] parts = input.Split(' ');
+                    char dzialanie = char.Parse(parts[0]);
+                    int a = int.Parse(parts[1]);
+                    int b = int.Parse(parts[2]);
+                    switch (dzialanie)
+                    {
+                        case '+': Console.WriteLine(a + b); break;
+                        case '-': Console.WriteLine(a - b); break;
+                        case '*': Console.WriteLine(a * b); break;
+                        case '/': Console.WriteLine(a / b); break;
+                        case '%': Console.WriteLine(a % b); break;
+                    }
+                }
+                else { koniec=true; }  
+            }
+        }
+    }
+}﻿
