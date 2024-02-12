1fac5319c445573009d14eee9a97257c1b51630a
Paweł Wachol <wachol1991@o2.pl>
2024-02-12 17:14:24 +0100

    12.02.2024

---
+++ Polski SPOJ/AL_15_01 - Test/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace AL_15_01___Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Darek, Jarek, Marek;
+            int max_pkt = 0;
+            Darek = Console.ReadLine();
+            Jarek = Console.ReadLine();
+            Marek = Console.ReadLine();
+            for (int i = 0; i < Darek.Length; i++)
+            {
+                if (Darek[i] != Marek[i] && Marek[i] == Jarek[i])
+                {
+                    max_pkt += 2;
+                }
+                if (Darek[i] != Jarek[i] && Marek[i] != Jarek[i] && Darek[i] != Marek[i])
+                {
+                    max_pkt++;
+                }
+                if ((Darek[i] == Marek[i] && Darek[i] != Jarek[i]) || (Darek[i] == Jarek[i] && Darek[i] != Marek[i]))
+                {
+                    max_pkt++;
+                }
+            }
+            Console.WriteLine(max_pkt);
+        }
+    }
+}
