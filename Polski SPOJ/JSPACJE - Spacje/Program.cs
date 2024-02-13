31ed9a91c7757b1e1e5d7c885da62e2a605f0c03
Paweł Wachol <wachol1991@o2.pl>
2024-02-13 15:39:30 +0100

    13.02.2024

---
+++ Polski SPOJ/JSPACJE - Spacje/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace JSPACE___Spacje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x;
+            while ((x = Console.ReadLine()) != null)
+            {
+                while (x.Contains(' '))
+                {
+                    int l = 0;
+                    l = x.Length;
+                    for (int i = 0; i < l - 1; i++)
+                    {
+                        if (x[i] == ' ')
+                        {
+                            x = x.Remove(i, 1);
+                            x = x.Substring(0, i) + char.ToUpper(x[i]) + x.Substring(i + 1);
+                            l--;
+                        }
+                    }
+                }
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
