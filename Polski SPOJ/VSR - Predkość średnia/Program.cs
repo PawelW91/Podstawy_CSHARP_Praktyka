5576ee34efefe8a54877eb29f2a38af7d306086f
Paweł Wachol <wachol1991@o2.pl>
2024-02-15 17:34:48 +0100

    15.02.2024

---
+++ Polski SPOJ/VSR - Predkość średnia/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace VSR___Predkość_średnia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ile;
+            int.TryParse(Console.ReadLine(), out ile);
+
+            for (int i = 0; i < ile; i++)
+            {
+                string line = Console.ReadLine();
+                string[] values = line.Split(' ');
+
+                int v1, v2;
+                int.TryParse(values[0], out v1);
+                int.TryParse(values[1], out v2);
+
+                Console.WriteLine((2 * (v1 * v2)) / (v1 + v2));
+            }
+        }
+    }
+}
