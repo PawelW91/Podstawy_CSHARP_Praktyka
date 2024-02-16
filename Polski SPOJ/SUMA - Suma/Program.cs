61eb1325678b2e3482382e2c0de5b8d2aba04088
Paweł Wachol <wachol1991@o2.pl>
2024-02-16 22:37:42 +0100

    16.02.2024

---
+++ Polski SPOJ/SUMA - Suma/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace SUMA___Suma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0, x;
+            while (int.TryParse(Console.ReadLine(), out x))
+            {
+                i += x;
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
