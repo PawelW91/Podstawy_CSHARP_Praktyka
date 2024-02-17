c00428fc933fe12a812f6d5715c4d0ddf3b675f0
Paweł Wachol <wachol1991@o2.pl>
2024-02-17 21:05:02 +0100

    17.02.2024

---
+++ Polski SPOJ/POL - Połowa/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace POL___Połowa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ile_t = int.Parse(Console.ReadLine());
+
+            while (ile_t-- > 0)
+            {
+                string x = Console.ReadLine();
+                int l = x.Length / 2;
+                x = x.Remove(l);
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
