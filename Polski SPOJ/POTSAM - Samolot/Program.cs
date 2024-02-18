b859391a4f92125997c46be05585a3ae9d77a2e8
Paweł Wachol <wachol1991@o2.pl>
2024-02-18 21:40:26 +0100

    18.02.2024

---
+++ Polski SPOJ/POTSAM - Samolot/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace POTSAM___Samolot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x = new int[4];
+            string[] input = Console.ReadLine().Split();
+
+            for (int i = 0; i < 4; i++)
+            {
+                x[i] = int.Parse(input[i]);
+            }
+
+            Console.WriteLine(x[0] * x[1] + x[2] * x[3]);
+        }
+    }
+}
