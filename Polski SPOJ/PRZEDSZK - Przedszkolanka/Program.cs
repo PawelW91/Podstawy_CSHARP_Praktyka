f59541b23ebb0e3727ea57ba41575278ec7185e2
Paweł Wachol <wachol1991@o2.pl>
2024-02-19 15:36:31 +0100

    19.02.2024

---
+++ Polski SPOJ/PRZEDSZK - Przedszkolanka/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace PRZEDSZK___Przedszkolanka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ile_t = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < ile_t; i++)
+            {
+                int a, b, NWW = 1, NWD=0, MIN=0;
+                string[] input = Console.ReadLine().Split();
+
+                if (input.Length != 2 || !int.TryParse(input[0], out a) || !int.TryParse(input[1], out b))
+                {
+                    return;
+                }
+
+                // NWW - najmniejsza wspolna wielokrotnosc
+                // NWD - najwiekszy wspolny dzielnik
+
+                MIN = Math.Min(a, b);
+                for (int j = 1; j <= MIN; j++)
+                {
+                    if (a % j == 0 && b % j == 0)
+                    {
+                        NWD = j;
+                    }
+                }
+
+                if (a == b)
+                {
+                    NWW = a;
+                }
+                else
+                {
+                    NWW = (a * b) / NWD;
+                }
+
+                Console.WriteLine(NWW);
+            }
+        }
+    }
+}
