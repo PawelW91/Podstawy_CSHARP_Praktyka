5eba290ec9424e4e602283aa46a54fc0113de72e
Paweł Wachol <wachol1991@o2.pl>
2024-02-21 14:16:10 +0100

    21.02.2024

---
+++ Polski SPOJ/PRIME_T - Liczby Pierwsze/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace PRIME_T___Liczby_Pierwsze
+{
+    class Program
+    {
+        static bool IsPrime(int a)
+        {
+            for (int i = 2; i < a; i++)
+            {
+                if (a % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static void Main()
+        {
+            int ile_t, x;
+            ile_t = int.Parse(Console.ReadLine());
+            for (int i = 0; i < ile_t; i++)
+            {
+                x = int.Parse(Console.ReadLine());
+                if (x < 2)
+                    Console.WriteLine("NIE");
+                else if (IsPrime(x))
+                    Console.WriteLine("TAK");
+                else
+                    Console.WriteLine("NIE");
+            }
+        }
+    }
+}
