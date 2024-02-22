b7d16475e61bd7710966f1d210db23353675a303
Paweł Wachol <wachol1991@o2.pl>
2024-02-22 15:01:12 +0100

    22.02.2024

---
+++ Polski SPOJ/RNO_DOD - Proste dodawanie/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace RNO_DOD___Proste_dodawanie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < t; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                int z = 0;
+
+                string[] numbers = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < n; j++)
+                {
+                    int x = int.Parse(numbers[j]);
+                    z += x;
+                }
+
+                Console.WriteLine(z);
+            }
+        }
+    }
+}
