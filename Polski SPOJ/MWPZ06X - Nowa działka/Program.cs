d8de9d343dc1c7a0d42bafede46c967f7a4bee92
Paweł Wachol <wachol1991@o2.pl>
2024-02-23 19:35:10 +0100

    23.02.2024

---
+++ Polski SPOJ/MWPZ06X - Nowa działka/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace MWPZ06X___Nowa_działka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ile_t = int.Parse(Console.ReadLine());
+            while (ile_t-- > 0)
+            {
+                int x = int.Parse(Console.ReadLine());
+                Console.WriteLine(x * x);
+            }
+        }
+    }
+}
