ef4203adbb5107c0234ead8d3a28552e12d40d27
Paweł Wachol <123093109+PawelW91@users.noreply.github.com>
2024-02-26 20:35:24 +0100

    26.02.2024

--- Polski SPOJ/PP0502B - Tablice/Program.cs
+++ Polski SPOJ/PP0502B - Tablice/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace PP0502B___Tablice
+namespace PP0502B___Tablice
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            int ilosc_prob = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < ilosc_prob; i++)
+            {
+                string[] line = Console.ReadLine().Split();
+                int ilosc_liczb = int.Parse(line[0]);
+                int[] tablica = new int[ilosc_liczb];
+
+                for (int j = 0; j < ilosc_liczb; j++)
+                {
+                    tablica[j] = int.Parse(line[j + 1]);
+                }
+
+                for (int k = ilosc_liczb - 1; k >= 0; k--)
+                {
+                    Console.Write(tablica[k]+" ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
