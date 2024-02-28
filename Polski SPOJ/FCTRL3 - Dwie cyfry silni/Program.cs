15f466f2f4890df6a735727b14a951c90737243c
Paweł Wachol <123093109+PawelW91@users.noreply.github.com>
2024-02-28 06:00:59 +0100

    28.02.2024

--- Polski SPOJ/FCTRL3 - Dwie cyfry silni/Program.cs
+++ Polski SPOJ/FCTRL3 - Dwie cyfry silni/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace FCTRL3___Dwie_cyfry_silni
+namespace FCTRL3___Dwie_cyfry_silni
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            int ile_t = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < ile_t; i++)
+            {
+                int l, d, j, x = 1;
+                l = int.Parse(Console.ReadLine());
+
+                if (l > 9)
+                {
+                    Console.WriteLine("0 0");
+                }
+                else
+                {
+                    for (int k = 1; k <= l; k++)
+                    {
+                        x *= k;
+                    }
+
+                    d = (x / 10) % 10;
+                    j = x % 10;
+                    Console.WriteLine($"{d} {j}");
+                }
+            }
         }
     }
 }
