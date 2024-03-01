2ddb93dd7e98b7527780c7b6b3edec31b7b67c34
Paweł Wachol <wachol1991@o2.pl>
2024-03-01 17:04:19 +0100

    01.03.2024

---
+++ Polski SPOJ/PP0601B - Podzielność/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace PP0601B___Podzielność
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ile_t = int.Parse(Console.ReadLine());
+
+            while (ile_t-- > 0)
+            {
+                string[] input = Console.ReadLine().Split();
+                int n = int.Parse(input[0]);
+                int x = int.Parse(input[1]);
+                int y = int.Parse(input[2]);
+
+                for (int i = 0; i < n; i++)
+                {
+                    if (i % x == 0 && i % y != 0)
+                    {
+                        Console.Write(i + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
