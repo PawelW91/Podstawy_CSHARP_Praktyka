1cf90351fac2ee1d67e265daf85ee42c488d3153
Paweł Wachol <wachol1991@o2.pl>
2024-03-03 13:17:20 +0100

    03.03.2024

---
+++ Polski SPOJ/PP0602A - Parzyste nieparzyste/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace PP0602A___Parzyste_nieparzyste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            for (int test = 0; test < t; test++)
+            {
+                string[] input = Console.ReadLine().Split();
+                int n = int.Parse(input[0]);
+                int[] numbers = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    numbers[i] = int.Parse(input[i + 1]);
+                }
+                for (int i = 1; i < n; i += 2)
+                {
+                    Console.Write(numbers[i] + " ");
+                }
+                for (int i = 0; i < n; i += 2)
+                {
+                    Console.Write(numbers[i] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
