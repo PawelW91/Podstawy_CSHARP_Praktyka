3c407c4ada20265195b73f99f68f746f06cd8ba7
Paweł Wachol <wachol1991@o2.pl>
2024-03-07 21:47:34 +0100

    07.03.2024

---
+++ Polski SPOJ/TABLICA - Tablica/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace TABLICA___Tablica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] numbers = input.Split(' ');
+
+            for (int i = numbers.Length - 1; i >= 0; i--)
+            {
+                int X = int.Parse(numbers[i]);
+                Console.Write(X + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
