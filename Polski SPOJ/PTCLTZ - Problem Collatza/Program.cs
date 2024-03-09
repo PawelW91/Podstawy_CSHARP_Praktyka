deca188a7be8d5f22c72897f6ea5b1ccb9d001cf
Paweł Wachol <wachol1991@o2.pl>
2024-03-09 18:53:14 +0100

    09.03.2024

---
+++ Polski SPOJ/PTCLTZ - Problem Collatza/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace PTCLTZ___Problem_Collatza
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ile_t = int.Parse(Console.ReadLine());
+            while (ile_t-- > 0)
+            {
+                int x = int.Parse(Console.ReadLine());
+                Console.WriteLine(znajdz_n(x));
+            }
+        }
+        static int znajdz_n(int x)
+        {
+            int n = 0;
+            while (x != 1)
+            {
+                if (x % 2 == 0)
+                {
+                    x = x / 2;
+                }
+                else
+                {
+                    x = 3 * x + 1;
+                }
+                n += 1;
+            }
+            return n;
+        }
+    }
+}
