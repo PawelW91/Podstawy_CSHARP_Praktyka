e1bb6b48f909b9afa41f78802d288eb9adddca34
Paweł Wachol <wachol1991@o2.pl>
2024-03-12 20:03:02 +0100

    12.03.2024

---
+++ Polski SPOJ/TRN - Transponowanie macierzy/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace TRN___Transponowanie_macierzy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int m = int.Parse(input[0]);
+            int n = int.Parse(input[1]);
+
+            int[,] x = new int[m, n];
+
+            for (int i = 0; i < m; i++)
+            {
+                string[] row = Console.ReadLine().Split();
+                for (int j = 0; j < n; j++)
+                {
+                    x[i, j] = int.Parse(row[j]);
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(x[j, i] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
