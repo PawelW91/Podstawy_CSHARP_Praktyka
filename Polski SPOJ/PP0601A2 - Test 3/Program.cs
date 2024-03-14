8cbeb8539e34da338e7281ca8e22b68152777295
Paweł Wachol <wachol1991@o2.pl>
2024-03-14 18:10:50 +0100

    14.03.2024

---
+++ Polski SPOJ/PP0601A2 - Test 3/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace PP0601A2___Test_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, ile = 0, yes = 0;
+            while (true)
+            {
+                x = int.Parse(Console.ReadLine());
+                Console.WriteLine(x);
+
+                if (x == 42)
+                {
+                    if (yes == 1)
+                    {
+                        ile++;
+                        yes = 0;
+                    }
+                }
+                else
+                {
+                    yes = 1;
+                }
+
+                if (ile > 2)
+                    break;
+            }
+        }
+    }
+}
