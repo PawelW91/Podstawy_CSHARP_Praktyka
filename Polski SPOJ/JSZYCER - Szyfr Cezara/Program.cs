672b757d2e424417a7af3bdce0c4df75acbf122d
Paweł Wachol <wachol1991@o2.pl>
2024-03-17 08:30:40 +0100

    17.03.2024

---
+++ Polski SPOJ/JSZYCER - Szyfr Cezara/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace JSZYCER___Szyfr_Cezara
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string x;
+            while ((x = Console.ReadLine()) != null)
+            {
+                foreach (char c in x)
+                {
+                    if (c >= 'X' && c <= 'Z')
+                    {
+                        Console.Write((char)(c - 23));
+                    }
+                    else if (c >= 'A' && c <= 'Z')
+                    {
+                        Console.Write((char)(c + 3));
+                    }
+                    else
+                    {
+                        Console.Write(c);
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
