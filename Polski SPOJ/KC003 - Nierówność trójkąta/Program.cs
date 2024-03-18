09ca67dfcc204268867766c44831d45c0f472f1e
Paweł Wachol <wachol1991@o2.pl>
2024-03-18 19:08:33 +0100

    18.03.2024

---
+++ Polski SPOJ/KC003 - Nierówność trójkąta/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace KC003___Nierówność_trójkąta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            while ((input = Console.ReadLine()) != null)
+            {
+                string[] tokens = input.Split(' ');
+
+                if (tokens.Length != 3)
+                {
+                    Console.WriteLine("Invalid input");
+                    continue; // Move to the next iteration of the loop
+                }
+
+                if (!float.TryParse(tokens[0], System.Globalization.NumberStyles.Float, System.Globalization.CultureInfo.InvariantCulture, out float a) ||
+                    !float.TryParse(tokens[1], System.Globalization.NumberStyles.Float, System.Globalization.CultureInfo.InvariantCulture, out float b) ||
+                    !float.TryParse(tokens[2], System.Globalization.NumberStyles.Float, System.Globalization.CultureInfo.InvariantCulture, out float c))
+                {
+                    Console.WriteLine("Invalid input");
+                    continue; // Move to the next iteration of the loop
+                }
+
+                if (a + b > c && a + c > b && b + c > a)
+                {
+                    Console.WriteLine("1");
+                }
+                else
+                {
+                    Console.WriteLine("0");
+                }
+            }
+        }
+    }
+}
