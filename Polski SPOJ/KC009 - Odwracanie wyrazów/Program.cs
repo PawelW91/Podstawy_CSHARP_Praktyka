f793d228b8ca94985919083484c0f2ebe1d2e834
Paweł Wachol <wachol1991@o2.pl>
2024-03-20 19:29:21 +0100

    20.03.2024

---
+++ Polski SPOJ/KC009 - Odwracanie wyrazów/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace KC009___Odwracanie_wyrazów
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string x;
+            while ((x = Console.ReadLine()) != null)
+            {
+                for (int i = x.Length - 1; i >= 0; i--)
+                {
+                    Console.Write(x[i]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
