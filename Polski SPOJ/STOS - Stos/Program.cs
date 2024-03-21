4cd9c66813d4253fb63d48a0a6d6b3b2a964c168
Paweł Wachol <wachol1991@o2.pl>
2024-03-21 22:02:47 +0100

    21.03.2024

---
+++ Polski SPOJ/STOS - Stos/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace STOS___Stos
+{
+    using System;
+    using System.Collections.Generic;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stos = new Stack<int>();
+            string polecenie;
+            int liczba;
+
+            while ((polecenie = Console.ReadLine()) != null)
+            {
+                if (polecenie == "-")
+                {
+                    if (stos.Count > 0)
+                    {
+                        Console.WriteLine(stos.Pop());
+                    }
+                    else
+                    {
+                        Console.WriteLine(":(");
+                    }
+                }
+                else if (polecenie == "+")
+                {
+                    if (stos.Count < 10 && int.TryParse(Console.ReadLine(), out liczba))
+                    {
+                        stos.Push(liczba);
+                        Console.WriteLine(":)");
+                    }
+                    else
+                    {
+                        Console.WriteLine(":(");
+                    }
+                }
+            }
+        }
+    }
+}
