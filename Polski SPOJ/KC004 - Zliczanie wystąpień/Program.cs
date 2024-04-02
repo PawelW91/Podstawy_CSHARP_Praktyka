f7123f60b4e69818cb35575cf46cfb2d98e42a38
Paweł Wachol <wachol1991@o2.pl>
2024-04-02 20:48:11 +0200

    02.04.2024

---
+++ Polski SPOJ/KC004 - Zliczanie wystąpień/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace KC004___Zliczanie_wystąpień
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            while ((line = Console.ReadLine()) != null)
+            {
+                string[] inputs = line.Split(' ');
+
+                int x = int.Parse(inputs[0]);
+                int y = int.Parse(inputs[1]);
+                int ile = 0;
+                for (int i = 0; i < y; i++)
+                {
+                    if (int.Parse(inputs[2 + i]) == x)
+                    {
+                        ile++;
+                    }
+                }
+                Console.WriteLine(ile);
+            }
+        }
+    }
+}
