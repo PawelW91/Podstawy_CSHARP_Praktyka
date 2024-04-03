db3cae860b2db238c9a8b9ad641564b5bdf24fe1
Paweł Wachol <wachol1991@o2.pl>
2024-04-03 19:04:48 +0200

    03.04.2024

---
+++ Polski SPOJ/JHTMLLET - Tagi HTML/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace JHTMLLET___Tagi_HTML
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string x;
+            while ((x = Console.ReadLine()) != null)
+            {
+                int l = x.Length;
+                for (int i = 1; i < l; i++)
+                {
+                    if (x[i - 1] == '<')
+                    {
+                        while (x[i] != '>')
+                        {
+                            x = x.Substring(0, i) + char.ToUpper(x[i]) + x.Substring(i + 1);
+                            i++;
+                        }
+                    }
+                }
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
