c8f333e573741fb7d6a25645fbdf486f2a5c4427
Paweł Wachol <wachol1991@o2.pl>
2024-04-04 19:32:15 +0200

    04.04.2024

---
+++ Polski SPOJ/MWPZ06D - Imieniny/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace MWPZ06D___Imieniny
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ile_t;
+            if (int.TryParse(Console.ReadLine(), out ile_t))
+            {
+                while (ile_t-- > 0)
+                {
+                    string[] input = Console.ReadLine().Split();
+                    int L = int.Parse(input[0]);
+                    int C = int.Parse(input[1]);
+                    L -= 1;
+                    if (L == 0)
+                    {
+                        Console.WriteLine("TAK");
+                    }
+                    else if (C % L == 0)
+                    {
+                        Console.WriteLine("NIE");
+                    }
+                    else
+                    {
+                        Console.WriteLine("TAK");
+                    }
+                }
+            }
+        }
+    }
+}
