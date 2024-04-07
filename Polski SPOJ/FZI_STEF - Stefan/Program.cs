b6f8acb10b9e269a4f0f43aee0761884c2e6da4c
Paweł Wachol <wachol1991@o2.pl>
2024-04-07 11:59:09 +0200

    07.04.2024

---
+++ Polski SPOJ/FZI_STEF - Stefan/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace FZI_STEF___Stefan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ile = int.Parse(Console.ReadLine());
+            long curSum = 0, maxSum = 0;
+            while (ile-- > 0)
+            {
+                int X = int.Parse(Console.ReadLine());
+                curSum += X;
+                if (curSum > maxSum) maxSum = curSum;
+                if (curSum < 0) curSum = 0;
+            }
+            Console.WriteLine(maxSum);
+        }
+    }
+}
